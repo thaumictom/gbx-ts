c01ba8583810efbf2a6d85840ac00efef5368f8e
thaumictom <tom@thaumictom.de>
2024-03-17 19:23:01 +0100

    Fix LZO handling for web version

--- src/Handlers/LZOHandler.ts
+++ src/Handlers/LZOHandler.ts
@@ -4,10 +4,12 @@ export default class LZOHandler {
 	 * @param data The data to compress.
 	 * @returns The compressed data as Buffer.
 	 */
-	static async compress(data: Array<number>): Promise<Buffer> {
+	static async compress(data: Array<number>): Promise<number[]> {
 		try {
-			const { LZO } = await import('lzo-ts');
-			return LZO.compress(data) as Buffer;
+			const module =
+				typeof window === 'undefined' ? await import('lzo-ts') : (window as any)['lzoTs'];
+
+			return module.LZO.compress(data);
 		} catch (error) {
 			console.error(error);
 			throw new Error('Failed to compress data, is the lzo-ts package installed?');
@@ -19,10 +21,12 @@ export default class LZOHandler {
 	 * @param data The data to decompress.
 	 * @returns The decompressed data as Buffer.
 	 */
-	static async decompress(data: Array<number>): Promise<Buffer> {
+	static async decompress(data: Array<number>): Promise<number[]> {
 		try {
-			const { LZO } = await import('lzo-ts');
-			return LZO.decompress(data) as Buffer;
+			const module =
+				typeof window === 'undefined' ? await import('lzo-ts') : (window as any)['lzoTs'];
+
+			return module.LZO.decompress(data);
 		} catch (error) {
 			console.error(error);
 			throw new Error('Failed to decompress data, is the lzo-ts package installed?');
