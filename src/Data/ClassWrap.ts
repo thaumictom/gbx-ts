df2139324dff11b493960b231ebd3841d8f792b0
thaumictom <tom@thaumictom.de>
2024-03-21 02:56:45 +0100

    Update classWrap with more wrappers

--- src/Data/ClassWrap.ts
+++ src/Data/ClassWrap.ts
@@ -1,4 +1,15 @@
 export const classWrap: { [DeprecatedClass: number]: number } = {
+	0x021080000: 0x03043000, // CGameCtnChallenge
+	0x02108d000: 0x03093000, // CGameCtnReplayRecord
+
 	0x024003000: 0x03043000, // CGameCtnChallenge
+	0x02400c000: 0x0305b000, // CGameCtnChallengeParameters
+	0x02401b000: 0x03092000, // CGameCtnGhost
+	0x02403a000: 0x03059000, // CGameCtnBlockSkin
+	0x02403c000: 0x0301b000, // CGameCtnCollectorList
+	0x02403f000: 0x03093000, // CGameCtnReplayRecord
+	0x024061000: 0x03078000, // CGameCtnMediaTrack
+	0x024062000: 0x03078000, // CGameCtnMediaTrack
+	0x024076000: 0x03079000, // CGameCtnMediaClip
 	0x02407e000: 0x03093000, // CGameCtnReplayRecord
 };
