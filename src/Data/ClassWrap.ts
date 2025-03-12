8477eb344dce6547aa0c886d662afbcc4f359279
thaumictom <tom@thaumictom.de>
2025-03-13 00:25:06 +0100

    Add comment to classwrap

--- src/Data/ClassWrap.ts
+++ src/Data/ClassWrap.ts
@@ -1,6 +1,6 @@
 export const classWrap: { [DeprecatedClass: number]: number } = {
-	0x021080000: 0x03043000, // CGameCtnChallenge
-	0x02108d000: 0x03093000, // CGameCtnReplayRecord
+	0x021080000: 0x03043000, // CGameCtnChallenge (VSkipper)
+	0x02108d000: 0x03093000, // CGameCtnReplayRecord (VSkipper)
 
 	0x024003000: 0x03043000, // CGameCtnChallenge
 	0x02400c000: 0x0305b000, // CGameCtnChallengeParameters
